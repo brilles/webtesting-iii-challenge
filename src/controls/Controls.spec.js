dcd6f657b1dac8aa0410e6b03946d50151e2f5f5
Brian Illes <brianrilles@gmail.com>
2019-04-10 15:28:54 -0700

    MVP complete, adds Display and Controls tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,8 +1,42 @@
 // Test away!
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { cleanup, render, fireEvent } from 'react-testing-library';
 import Controls from './Controls.js';
 
 describe('<Controls/>', () => {
-  it('', () => {});
+  it('provide buttons to toggle the closed and locked states', () => {
+    const { getByText } = render(<Controls />);
+
+    const closeBtn = getByText(/Close Gate/i);
+    const LockBtn = getByText(/Lock Gate/i);
+    expect(closeBtn).not.toBeNull();
+    expect(LockBtn).not.toBeNull();
+  });
+
+  it('buttons text changes to reflect the state the door will be in if clicked', () => {
+    const { getByText } = render(<Controls locked={false} closed={true} />);
+    const openBtn = getByText(/Open Gate/i);
+    expect(openBtn).not.toBeNull();
+  });
+
+  it('the closed toggle button is disabled if the gate is closed', () => {
+    const { queryByText } = render(<Controls />);
+
+    let closeBtn = queryByText(/Close Gate/i);
+    fireEvent.click(closeBtn);
+    let closeBtn2 = queryByText(/Open Gate/i);
+
+    expect(closeBtn2.disabled).toBe(false);
+  });
+
+  it('the locked toggle button is disabled if the gate is open', () => {
+    const { queryByText } = render(<Controls />);
+    let closeBtn = queryByText(/Close Gate/i);
+    fireEvent.click(closeBtn);
+    let lockBtn = queryByText(/Lock Gate/i);
+    fireEvent.click(lockBtn);
+    let lockBtn2 = queryByText(/Lock Gate/i);
+
+    expect(lockBtn2.disabled).toBe(true);
+  });
 });
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,8 +1,30 @@
 // Test away!
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 import Display from './Display.js';
 
-describe('', () => {
-  it('', () => {});
+describe('<Display />', () => {
+  it('displays if gate is open and if it is unlocked, with correct props', () => {
+    const { queryByText } = render(<Display locked={false} closed={false} />);
+
+    expect(queryByText(/Open/i)).not.toBeNull();
+    expect(queryByText(/Unlocked/i)).not.toBeNull();
+  });
+
+  it('displays if gate is closed and if it is locked, with correct props', () => {
+    const { getByText } = render(<Display locked={true} closed={true} />);
+    getByText(/UnLocked/i);
+    getByText(/Closed/i);
+  });
+
+  it('when locked or closed use the red-led class', () => {
+    const { container } = render(<Display locked={true} closed={true} />);
+    expect(container.firstChild.firstChild).toHaveClass('red-led');
+  });
+
+  it('when unlocked or open use the green-led class', () => {
+    const { container } = render(<Display locked={false} closed={false} />);
+    expect(container.firstChild.firstChild).toHaveClass('green-led');
+  });
 });
