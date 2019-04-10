d90fb842d14df6c0464d792d1ecf28a6e1b39021
Brian Illes <brianrilles@gmail.com>
2019-04-10 12:16:59 -0700

    Initial commit, adds snapshots

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,15 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { cleanup } from 'react-testing-library';
+import Dashboard from './Dashboard.js';
+
+afterEach(() => {
+  cleanup();
+});
+
+describe('<Dashboard />', () => {
+  it('matches snapshot', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
