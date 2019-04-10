50cce0909c96250dbfd26ee41c20f2676c21f3d2
Brian Illes <brianrilles@gmail.com>
2019-04-10 13:30:26 -0700

    adds Dashboard tests

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Controls from './Controls.js';
+
+describe('<Controls/>', () => {
+  it('', () => {});
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,8 @@
 // Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Display from './Display.js';
+
+describe('', () => {
+  it('', () => {});
+});
